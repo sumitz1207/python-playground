38b00419673f9fdee271fe2a77edc1a668528686
Sumit Patel <sumitz1207@gmail.com>
2023-06-17 22:00:28 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
+import App from "./component/App";
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
